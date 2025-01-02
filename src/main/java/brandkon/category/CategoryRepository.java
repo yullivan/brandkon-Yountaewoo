68ad863b2094f470e76f04b95123189f92381c8a
YounTaeWoo <tty7797@naver.com>
2025-01-02 15:30:19 +0900

    feat: 카테코리 이름으로 카테고리 찾는 query method 구현

--- src/main/java/brandkon/category/CategoryRepository.java
+++ src/main/java/brandkon/category/CategoryRepository.java
@@ -3,4 +3,5 @@ package brandkon.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
 }
