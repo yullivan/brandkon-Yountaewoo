c71a53840790262f2b2c0830786d25913d908d00
YounTaeWoo <tty7797@naver.com>
2025-01-02 13:32:02 +0900

    feat: 카테고리 목록 조회하는 함수 구현

--- src/main/java/brandkon/category/CategoryService.java
+++ src/main/java/brandkon/category/CategoryService.java
@@ -1,4 +1,26 @@
 package brandkon.category;
 
+import brandkon.category.DTO.CategoryResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    // 카테고리 목록 조회
+    public List<CategoryResponseDto> findCategories() {
+        List<Category> all = categoryRepository.findAll();
+        List<CategoryResponseDto> finds = all.stream()
+                .map(category -> new CategoryResponseDto(category.getId(), category.getName(), category.getSlug()
+                        , category.getImageUrl()))
+                .toList();
+        return finds;
+    }
 }
