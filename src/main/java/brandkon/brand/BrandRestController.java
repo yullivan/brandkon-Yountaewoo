b8e428eb7884a472dfa3156c12978379c99ca899
YounTaeWoo <tty7797@naver.com>
2025-01-02 13:39:23 +0900

    feat: 브랜드 목록 조회 구현

--- src/main/java/brandkon/brand/BrandRestController.java
+++ src/main/java/brandkon/brand/BrandRestController.java
@@ -1,4 +1,23 @@
 package brandkon.brand;
 
+import brandkon.brand.DTO.BrandResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BrandRestController {
+
+    private BrandService brandService;
+
+    public BrandRestController(BrandService brandService) {
+        this.brandService = brandService;
+    }
+
+    // 브랜드 목록 조회
+    @GetMapping("/brands")
+    public List<BrandResponseDto> findBrands() {
+        return brandService.findBrands();
+    }
 }
--- src/main/java/brandkon/brand/BrandService.java
+++ src/main/java/brandkon/brand/BrandService.java
@@ -1,4 +1,25 @@
 package brandkon.brand;
 
+import brandkon.brand.DTO.BrandResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BrandService {
+
+    private final BrandRepository brandRepository;
+
+    public BrandService(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+
+    //브랜드 목록 조회
+    public List<BrandResponseDto> findBrands() {
+        List<Brand> all = brandRepository.findAll();
+        List<BrandResponseDto> finds = all.stream()
+                .map(brand -> new BrandResponseDto(brand.getId(), brand.getName(), brand.getImageUrl()))
+                .toList();
+        return finds;
+    }
 }
