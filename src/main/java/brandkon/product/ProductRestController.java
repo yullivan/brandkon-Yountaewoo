17f2d3fe2f9893f8b46c9a107846708d15fa7fd5
YounTaeWoo <tty7797@naver.com>
2025-01-02 14:07:29 +0900

    feat: 상품 목록 조회

--- src/main/java/brandkon/product/ProductRestController.java
+++ src/main/java/brandkon/product/ProductRestController.java
@@ -1,4 +1,23 @@
 package brandkon.product;
 
+import brandkon.product.DTO.ProductResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ProductRestController {
+
+    private ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    //상품 목록 조회
+    @GetMapping("/products")
+    public List<ProductResponseDto> findAll() {
+        return productService.findALl();
+    }
 }
--- src/main/java/brandkon/product/ProductService.java
+++ src/main/java/brandkon/product/ProductService.java
@@ -1,4 +1,26 @@
 package brandkon.product;
 
+import brandkon.product.DTO.ProductResponseDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //상품 목록 조회
+    public List<ProductResponseDto> findALl() {
+        List<Product> finds = productRepository.findAll();
+        List<ProductResponseDto> findALl = finds.stream()
+                .map(product -> new ProductResponseDto(product.getId(), product.getBrand().getName(),
+                        product.getName(), product.getPrice(), product.getImageUrl()))
+                .toList();
+        return findALl;
+    }
 }
