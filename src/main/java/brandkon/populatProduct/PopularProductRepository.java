48e5e8a1a597b1dd278f10c181bff13c6edea551
YounTaeWoo <tty7797@naver.com>
2025-01-02 17:47:32 +0900

    feat: 필요없는것 삭제

--- src/main/java/brandkon/populatProduct/PopularProductRepository.java
+++
@@ -1,6 +0,0 @@
-package brandkon.populatProduct;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PopularProductRepository extends JpaRepository<PopulatProduct, Long> {
-}
