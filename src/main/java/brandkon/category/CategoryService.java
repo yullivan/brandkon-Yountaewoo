0e5257cd6a148663f407ef2d7720f23eca732e77
YounTaeWoo <tty7797@naver.com>
2025-01-09 17:22:25 +0900

    feat: 새로운 변수 선언 안하고 바로 return

--- src/main/java/brandkon/category/CategoryService.java
+++ src/main/java/brandkon/category/CategoryService.java
@@ -17,10 +17,9 @@ public class CategoryService {
     // 카테고리 목록 조회
     public List<CategoryResponseDto> findCategories() {
         List<Category> all = categoryRepository.findAll();
-        List<CategoryResponseDto> finds = all.stream()
+        return all.stream()
                 .map(category -> new CategoryResponseDto(category.getId(), category.getName(), category.getSlug()
                         , category.getImageUrl()))
                 .toList();
-        return finds;
     }
 }
